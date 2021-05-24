360963e4505f1d8acb0c0fb22c2fbfe783e4ea51
Henrique JensenD <henriquejensen@hotmail.com>
2021-05-24 19:01:59 -0300

    createStudent

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -43,7 +43,7 @@ describe('#circle', () => {
     assert.strictEqual(area, 28.26);
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    const circleRadius3 = { radius: 3, area: 28.259999999999998, circumference: 18.84 };
+    const circleRadius3 = { radius: 3, area: 3.14 * 3 * 3, circumference: 18.84 };
     assert.deepStrictEqual(circle(3), circleRadius3);
   });
 });
